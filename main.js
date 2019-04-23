bf6c68c4b100a487726c8ec97360fb115aab50ec
Иван Симонов <satihrom@gmail.com>
2019-04-23 21:33:47 +0300

    это мой первый пробный комит

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+//alert("Привет мир,это код со страницы main.js");
+var inc = 10,
+    dec = 10;
+
+
+console.log(++inc);
+
+console.log(inc++);
+console.log(inc);
+console.log(dec--);
+console.log(dec);
+
