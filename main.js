3c69746bd17d2c402bd58dd9847c33b0b318e6d0
Иван Симонов <satihrom@gmail.com>
2019-05-11 19:53:44 +0300

    некоторые изменения

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 'use strict';
 //alert("Привет мир,это код со страницы main.js");
-var inc = 10,
-    dec = 10;
 
 
-console.log(++inc);
 
-console.log(inc++);
-console.log(inc);
-console.log(dec--);
-console.log(dec);
+
+alert(0 || "" || 2 || undefined || true || falsе);
+
+
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+for (let i=1;i<6;i++){
+    if(i==3){
+        continue
+    }
+    console.log('иван');
+}
