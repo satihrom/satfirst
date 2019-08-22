81747059aeabd2069f91657630d5e98213d40636
Ivan <satihrom@gmail.com>
2019-08-22 13:30:08 +0300

    готовые слайдеры первый срабатывающие на соответствующие дивы

--- практика JS/main.js
+++ практика JS/main.js
@@ -1,6 +1,7 @@
 
-
 /*создание слайдера*/
+
+
 let line=document.querySelector('.line');
 let slides=document.querySelectorAll('.slide');
 let sliderWidth=document.querySelector('.slider').offsetWidth;
@@ -9,16 +10,18 @@ let lineWidth = 0;
 let offset=0;
 let step=0;
 let tostop=0;
+//var a = document.getElementById('line');
 
 
 for (let i=0; i<slides.length; i++){
     widthArray.push(slides[i].offsetWidth);
     lineWidth += slides[i].offsetWidth;
+    
 }
 line.style.width = lineWidth+'px';
 
 
-document.onclick=function slidestep (){
+function slidestep (){
     tostop=lineWidth-sliderWidth-(offset+widthArray[step]);
     if (tostop>=0){offset=offset+widthArray[step];
     line.style.left=-offset+'px';
@@ -38,3 +41,45 @@ document.onclick=function slidestep (){
     
 }
 
+
+/*создание замкнутого слайдера*/
+let slids=document.querySelectorAll('.slide-single');
+console.log(slids);
+let slid=[];
+for(let i=0; i<slids.length; i++){
+    slid[i]=slids[i].src;
+    slids[i].remove();
+}
+console.log(slid);
+let stp=0;
+let oset=0;
+function draw(){
+    let img=document.createElement('img');
+    img.src=slid[stp];
+    img.classList.add('slide-single');
+    img.style.left=oset*256 + 'px';
+    document.querySelector('#sld').appendChild(img);
+    if (stp+1 == slid.length){
+        stp=0;
+    }
+    else{
+        stp++;
+    }
+    oset=1;
+}
+function left(){
+    sld.onclick=null;
+    let slids2=document.querySelectorAll('.slide-single');
+    let slid2=0;
+    for (let i=0; i<slids2.length; i++){
+        slids2[i].style.left=slid2*256-256 +'px';
+        slid2++;
+    }
+    setTimeout(function(){
+        slids2[0].remove();
+        draw();
+        sld.onclick=left;
+    }, 1000);
+}
+draw();draw();
+sld.onclick=left;
