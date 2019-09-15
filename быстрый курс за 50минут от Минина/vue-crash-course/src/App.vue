87397e84e826c44501bf7e9f595a83a48f0a1385
Ivan <satihrom@gmail.com>
2019-09-15 14:51:36 +0300

    не запускается приложение выдаются ошибки

--- быстрый курс за 50минут от Минина/vue-crash-course/src/App.vue
+++ быстрый курс за 50минут от Минина/vue-crash-course/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To do aplication</h1>
+  
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
