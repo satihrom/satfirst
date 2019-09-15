540ee03b98f3e9f1f43250ef6c70f683f261521a
Ivan <satihrom@gmail.com>
2019-09-15 22:07:38 +0300

    директивы v-bind короткая запись является двоеточие перед названием атрибута

--- vue.projects/test-from-deka/src/App.vue
+++ vue.projects/test-from-deka/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
+    <TodoList :todos='todos' />
      
     
   </div>
 </template>
 
 <script>
+import TodoList from'./components/TodoList.vue'
 
 
 export default {
   name: 'app',
+  data(){
+      return{
+        todos['Ремонт']
+      }
+  },
+  components:{
+    TodoList
+  }
 }
 </script>
 
